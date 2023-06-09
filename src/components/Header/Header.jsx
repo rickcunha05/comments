30ea439374893c6482462766a3cc9068ea201a74
Henrique Cunha <106562630+rickcunha05@users.noreply.github.com>
2023-06-09 14:56:58 -0300

    created header and implementation constants

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
 import styles from "./Header.module.css";
+import images from "../../constants/images";
 
 export function Header() {
   return (
     <header className={styles.header}>
+      <img src={images.logo} />
       <div>Ignite Feed</div>
     </header>
   );
