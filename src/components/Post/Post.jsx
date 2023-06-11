737ec6aec2c58bcb391f9fc26614bc7a67079320
Henrique Cunha <106562630+rickcunha05@users.noreply.github.com>
2023-06-10 22:16:21 -0300

    add focus green in global css and animated button!

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -21,7 +21,7 @@ export default function Post() {
       <div className={styles.content}>
         <p> Fala galeraa 👋</p>
         <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          Acabei de subir mais um projeto no meu portfolio. É um projeto que fiz
           no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
         </p>
         <p>👉 jane.design/doctorcare</p>
@@ -31,6 +31,13 @@ export default function Post() {
           <a href=""> #rocketseat </a>
         </p>
       </div>
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
